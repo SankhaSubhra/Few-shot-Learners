671e9b300eab64699e8cb865f56194b98d0865f5
Sankha Subhra Mullick <mullicksankhasubhra@gmail.com>
2020-09-25 15:50:20 +0530

    Update run_supervised_meta.sh

--- run_supervised_meta.sh
+++ run_supervised_meta.sh
@@ -8,7 +8,7 @@ python -u run_miniimagenet_maml.py --shots 1 --meta-shots 15 --inner-iters 5 --m
 python -u run_omniglot_maml.py --shots 1 --meta-shots 1 --inner-iters 1 --meta-step 0.001 --meta-batch 32 --meta-iters 60000 --eval-iters 10 --learning-rate 0.4 --eval-samples 600 --order 2 | tee output_omniglot_maml_order2_seed0.txt
 
 # REPTILE miniimagenet
-python -u run_miniimagenet_reptile.py --seed 0 --shots 1 --inner-batch 10 --inner-iters 8 --meta-step 1 --meta-batch 5 --meta-iters 10000 --eval-batch 5 --eval-iters 50 --eval-samples 1000 --learning-rate 0.001 --train-shots 15 | tee reptile_miniimagenet_seed0.txt
+python -u run_miniimagenet_reptile.py --seed 0 --shots 1 --inner-batch 10 --inner-iters 8 --meta-step 1 --meta-batch 5 --meta-iters 100000 --eval-batch 5 --eval-iters 50 --eval-samples 1000 --learning-rate 0.001 --train-shots 15 | tee reptile_miniimagenet_seed0.txt
 
 # REPTILE omniglot
 python -u run_omniglot_reptile.py --seed 0 --shots 1 --inner-batch 10 --inner-iters 5 --meta-step 1 --meta-batch 5 --meta-iters 100000 --eval-batch 5 --eval-iters 50 --eval-samples 1000 --learning-rate 0.001 --train-shots 10 | tee reptile_omniglot_seed0.txt
